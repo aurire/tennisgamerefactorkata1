69b4b62b0e0906d74d9a473cfafe199fd5d8174c
Rex von Aurum <frenkas@gmail.com>
2020-06-05 12:52:39 +0300

    KATA-02: some in-place refactoring. No-SOLID

--- TennisGame1.php
+++ TennisGame1.php
@@ -1,81 +1,73 @@
 <?php
 
+/**
+ * Class TennisGame1
+ */
 class TennisGame1 implements TennisGame
 {
-    private $m_score1 = 0;
-    private $m_score2 = 0;
+    const SCORE_NAMES = [
+        0 => 'Love',
+        1 => 'Fifteen',
+        2 => 'Thirty',
+        3 => 'Forty',
+    ];
+    const SCORE_DEUCE = 'Deuce';
+    const ID_PLAYER_1 = 'player1';
+
+    private $playerScore1 = 0;
+    private $playerScore2 = 0;
     private $player1Name = '';
     private $player2Name = '';
 
-    public function __construct($player1Name, $player2Name)
+    /**
+     * TennisGame1 constructor.
+     * @param string $player1Name
+     * @param string $player2Name
+     */
+    public function __construct(string $player1Name, string $player2Name)
     {
         $this->player1Name = $player1Name;
         $this->player2Name = $player2Name;
     }
 
+    /**
+     * @param string $playerName
+     */
     public function wonPoint($playerName)
     {
-        if ('player1' == $playerName) {
-            $this->m_score1++;
+        if (self::ID_PLAYER_1 == $playerName) {
+            $this->playerScore1++;
         } else {
-            $this->m_score2++;
+            $this->playerScore2++;
         }
     }
 
+    /**
+     * @return string
+     */
     public function getScore()
     {
-        $score = "";
-        if ($this->m_score1 == $this->m_score2) {
-            switch ($this->m_score1) {
-                case 0:
-                    $score = "Love-All";
-                    break;
-                case 1:
-                    $score = "Fifteen-All";
-                    break;
-                case 2:
-                    $score = "Thirty-All";
-                    break;
-                default:
-                    $score = "Deuce";
-                    break;
-            }
-        } elseif ($this->m_score1 >= 4 || $this->m_score2 >= 4) {
-            $minusResult = $this->m_score1 - $this->m_score2;
-            if ($minusResult == 1) {
-                $score = "Advantage player1";
-            } elseif ($minusResult == -1) {
-                $score = "Advantage player2";
-            } elseif ($minusResult >= 2) {
-                $score = "Win for player1";
-            } else {
-                $score = "Win for player2";
-            }
-        } else {
-            for ($i = 1; $i < 3; $i++) {
-                if ($i == 1) {
-                    $tempScore = $this->m_score1;
+        $max = $this->playerScore1 > $this->playerScore2 ? $this->playerScore1 : $this->playerScore2;
+        $wins = $this->playerScore1 === $this->playerScore2 ? 0 : ($this->playerScore1 > $this->playerScore2 ? 1 : 2);
+        if ($max < 4) {
+            if (0 === $wins) {
+                if (3 === $max) {
+                    return self::SCORE_DEUCE;
                 } else {
-                    $score .= "-";
-                    $tempScore = $this->m_score2;
+                    return static::SCORE_NAMES[$max] . '-All';
                 }
-                switch ($tempScore) {
-                    case 0:
-                        $score .= "Love";
-                        break;
-                    case 1:
-                        $score .= "Fifteen";
-                        break;
-                    case 2:
-                        $score .= "Thirty";
-                        break;
-                    case 3:
-                        $score .= "Forty";
-                        break;
-                }
+            } else {
+                return static::SCORE_NAMES[$this->playerScore1] . '-' . static::SCORE_NAMES[$this->playerScore2];
             }
         }
-        return $score;
+        if (0 === $wins) {
+            return self::SCORE_DEUCE;
+        }
+        $difference = abs($this->playerScore1 - $this->playerScore2);
+        if ($difference > 1) {
+            return 'Win for player' . $wins;
+        }
+
+        return 'Advantage player' . $wins;
     }
 }
-
